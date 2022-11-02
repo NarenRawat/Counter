c869a1c36ca191e700d4e6e78d54bff53423bc48
NarenRawat <86599661+NarenRawat@users.noreply.github.com>
2022-11-03 00:18:02 +0530

    feat(lock_screen): Make keypad in lock_screen

--- libs/uix/baseclass/lock_screen.py
+++ libs/uix/baseclass/lock_screen.py
@@ -1,4 +1,40 @@
+from kivymd.app import MDApp
+from kivy.clock import Clock
+from kivymd.uix.button import MDFlatButton, MDIconButton
+from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.widget import MDWidget
+from kivy.properties import ColorProperty
+
+
+class Keypad(MDGridLayout):
+
+    def __init__(self, *args, **kwargs):
+        super(Keypad, self).__init__(*args, **kwargs)
+        Clock.schedule_once(self._init_keys)
+
+    def _init_keys(self, dt):
+        for i in range(1, 10):
+            self.add_widget(TextKey(text=str(i)))
+
+        self.add_widget(TextKey(text="C"))
+        self.add_widget(TextKey(text="0"))
+        self.add_widget(IconKey(icon="backspace-outline"))
+
+
+class TextKey(MDFlatButton):
+    key_color = ColorProperty((1, 0, 0, 1))
+
+    def __init__(self, *args, **kwargs):
+        super(TextKey, self).__init__(*args, **kwargs)
+        self.rounded_button = True
+
+
+class IconKey(MDIconButton):
+    key_color = ColorProperty((1, 0, 0, 1))
+
+    def __init__(self, *args, **kwargs):
+        super(IconKey, self).__init__(*args, **kwargs)
 
 
 class LockScreen(MDScreen):
