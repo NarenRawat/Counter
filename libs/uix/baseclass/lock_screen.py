08c19f770a11cc3eefc987812fcbcec7354040ff
NarenRawat <86599661+NarenRawat@users.noreply.github.com>
2022-11-03 23:50:44 +0530

    fix: add password entry field in lock screen

--- libs/uix/baseclass/lock_screen.py
+++ libs/uix/baseclass/lock_screen.py
@@ -1,10 +1,11 @@
-from kivymd.app import MDApp
 from kivy.clock import Clock
+from kivy.properties import (BooleanProperty, ColorProperty, NumericProperty,
+                             ObjectProperty, OptionProperty, StringProperty)
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDFlatButton, MDIconButton
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.widget import MDWidget
-from kivy.properties import ColorProperty
 
 
 class Keypad(MDGridLayout):
@@ -15,11 +16,23 @@ class Keypad(MDGridLayout):
 
     def _init_keys(self, dt):
         for i in range(1, 10):
-            self.add_widget(TextKey(text=str(i)))
+            self.add_widget(TextKey(text=str(i),
+                                    on_release=self.num_key_press))
 
-        self.add_widget(TextKey(text="C"))
-        self.add_widget(TextKey(text="0"))
-        self.add_widget(IconKey(icon="backspace-outline"))
+        self.add_widget(TextKey(text="C", on_release=self.clear_key_press))
+        self.add_widget(TextKey(text="0", on_release=self.num_key_press))
+        self.add_widget(
+            IconKey(icon="backspace-outline",
+                    on_release=self.backspace_key_press))
+
+    def num_key_press(self, *args):
+        pass
+
+    def clear_key_press(self, *args):
+        pass
+
+    def backspace_key_press(self, *args):
+        pass
 
 
 class TextKey(MDFlatButton):
@@ -37,7 +50,73 @@ class IconKey(MDIconButton):
         super(IconKey, self).__init__(*args, **kwargs)
 
 
+class PasswordEntry(MDBoxLayout):
+    pass_length = NumericProperty(4)
+
+    def __init__(self, *args, **kwargs):
+        super(PasswordEntry, self).__init__(*args, **kwargs)
+        Clock.schedule_once(self._init_components)
+
+    def _init_components(self, dt):
+        for i in range(self.pass_length):
+            self.add_widget(PasswordEntryDot())
+
+
+class PasswordEntryDot(MDWidget):
+    enabled = BooleanProperty(False)
+
+    def __init__(self, *args, **kwargs):
+        super(PasswordEntryDot, self).__init__(*args, **kwargs)
+
+
 class LockScreen(MDScreen):
+    _pass_entry = ObjectProperty(None)
+
+    pass_length = NumericProperty(6)
+    next_screen_name = StringProperty()
+    correct_password = StringProperty()
+    mode = OptionProperty("signin", options=["signin", "signup"])
+
+    _entered_pass = StringProperty("")
 
     def __init__(self, *args, **kwargs):
         super(LockScreen, self).__init__(*args, **kwargs)
+
+    def on__entered_pass(self, *args):
+        password = args[1]
+        password_length = len(password)
+        children_list = list(reversed(self._pass_entry.children))
+
+        for index, child in enumerate(children_list):
+            if index < password_length:
+                child.enabled = True
+            else:
+                child.enabled = False
+
+    def pass_validate(self):
+        print(self._entered_pass, self.correct_password)
+
+        return self._entered_pass == self.correct_password
+
+    def switch_to_next_screen(self):
+        if self.next_screen_name:
+            self.parent.current = self.next_screen_name
+        else:
+            self.parent.current = self.parent.next()
+
+    def num_key_press(self, obj):
+        entered_pass_length = len(self._entered_pass)
+        if entered_pass_length == self.pass_length - 1:
+            self._entered_pass += obj.text
+            matched = self.pass_validate()
+            if matched:
+                self.switch_to_next_screen()
+            self.clear_pass()
+        elif entered_pass_length < self.pass_length:
+            self._entered_pass += obj.text
+
+    def clear_pass(self, *args):
+        self._entered_pass = ""
+
+    def backspace_key_press(self, obj):
+        self._entered_pass = self._entered_pass[:-1]
--- libs/uix/baseclass/root.py
+++ libs/uix/baseclass/root.py
@@ -17,7 +17,15 @@ class Root(MDScreenManager):
 
         for screen_name in screens.keys():
             screen_details = screens[screen_name]
-            exec(screen_details["import"])
-            screen_object = eval(screen_details["object"])
+            exec(screen_details.get("import"))
+
+            screen_options = screen_details.get("options", {})
+            print(screen_name, screen_options)
+
+            screen_object_str = screen_details.get("object")
+            screen_object_str = screen_object_str.replace(
+                "()", "(**screen_options)")
+            screen_object = eval(screen_object_str)
             screen_object.name = screen_name
+
             self.add_widget(screen_object)
